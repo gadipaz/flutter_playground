56be28908ca84bfd886c700d50817c6bda11855d
Gadi <gadi@riseco.ai>
2024-04-02 15:34:23 +0300

    done

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_playground/actions/actions.dart';
 import 'package:flutter_playground/models/app_state.dart';
 import 'package:flutter_playground/reducers/app_reducer.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:qr_flutter_new/qr_flutter.dart';
 import 'package:redux/redux.dart';
 
 void main() {
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
       title: 'Playground',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        brightness: Brightness.dark,
+        //brightness: Brightness.dark,
         primaryColor: Colors.blueGrey,
       ),
       home: Scaffold(
@@ -42,6 +43,26 @@ class MyApp extends StatelessWidget {
                     child: Text('Dispatch the action'),
                     onPressed: () => StoreProvider.of<AppState>(context)
                         .dispatch(SomeAction(!reduxSetup)),
+                  ),
+                  QrImageView(
+                    data: 'https://www.ynet.co.il',
+                    version: QrVersions.auto,
+                    foregroundColor: Colors.blue,
+                    size: 320,                    
+                    eyeStyle: const QrEyeStyle(borderRadius: 10),
+                    // dataModuleStyle: const QrDataModuleStyle(
+                    //   borderRadius: 5,
+                    //   roundedOutsideCorners: true,
+                    // ),
+                    embeddedImage: const AssetImage('assets/images/bird.png'),
+                    embeddedImageStyle: QrEmbeddedImageStyle(
+                      size: Size.square(60),
+                      shapeColor: Colors.white,
+                      safeArea: true,
+                      // safeAreaMultiplier: 1.05,
+                      embeddedImageShape: EmbeddedImageShape.square,
+                      borderRadius: 10,
+                    ),
                   ),
                 ],
               ),
